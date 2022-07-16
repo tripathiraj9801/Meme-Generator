34633850d7de69f65e176e77649ebf951da1d741
Rajeev Tripathi <tripathirajeev9801@gmail.com>
2022-07-16 19:06:46 +0530

    initial commit

---
+++ MemeGenerator.js
@@ -0,0 +1,31 @@
+import { javascript, JavascriptModulesPlugin, LibManifestPlugin } from "webpack"
+LibManifestPlugin()
+
+{
+    CDATASection; 
+    caches; 
+    Float32Array;
+
+    navigator; 
+    main()
+    {
+        this encodeURIComponent
+        webkitURL
+        if (webkitURL == null)
+            {
+                DelayNode; 
+            }
+
+
+            JavascriptModules Plugin; 
+            clearImmediate; 
+
+        JavascriptModulesPlugin;
+        clearTimeout
+
+        a= 10; 
+        console.log(a); 
+        javascript; 
+    }
+    
+}  
---
+++ README.md
@@ -0,0 +1,20 @@
+# React Meme Generator
+
+This react app was built from scratch without any scaffolding code.
+
+![](demo.png)
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
